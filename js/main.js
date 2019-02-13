8021665e8f776cc605c1d89fcd5fbb4fbd07e15d
Blake Bless <blakebless7@gmail.com>
2019-02-13 15:49:09 -0800

    use computed property for stock

--- js/main.js
+++ js/main.js
@@ -3,40 +3,43 @@ var app = new Vue({
     data: {
         brand: 'The Best',
         product: 'Socks',
-        image: 'images/socks-bl.png',
-        inStock: true,
         details: ["100% cotton", "Gender-Neutral", "Strong"],
+        selectedVariant: 0,
+        cart: 0,
         variants: [
             {
                 sku:1234,
                 feel:"Soft",
                 type:"Long",
                 color:"#329f65",
-                image:"images/socks-grn.png",
+                variantImage:"images/socks-grn.png",
+                variantQuantity: 2
             },
             {
                 sku:1235,
                 feel:"Rough",
                 type:"Tough",
                 color:"#9f4235",
-                image:"images/socks-red.png",
+                variantImage:"images/socks-red.png",
+                variantQuantity: 30
             },
             {
                 sku:1236,
                 feel:"Smooth",
                 type:"Comfort",
                 color:"#3581a0",
-                image:"images/socks-bl.png",
+                variantImage:"images/socks-bl.png",
+                variantQuantity: 0
             }
         ],
-        cart: 0,
     },
     methods: {
         addToCart: function(){
             this.cart += 1;
         },
-        setColor: function(variantImage){
-            this.image = variantImage;
+        updateProduct: function(index){
+            this.selectedVariant = index;
+            console.log(index);
         },
         removeFromCart: function(){
             if(this.cart != 0){
@@ -47,6 +50,16 @@ var app = new Vue({
     computed: {
         title() {
             return this.brand + ' ' + this.product;
-        }
+        },
+        image(){
+            return this.variants[this.selectedVariant].variantImage;
+        },
+        inStock(){
+            if (this.variants[this.selectedVariant].variantQuantity < 1){
+                return false;
+            } else {
+                return true;
+            }
+        },
     }
 });
